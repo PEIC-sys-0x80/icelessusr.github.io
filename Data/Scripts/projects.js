c4cd7c6e303c7e5e7a01b9cc5a7e283f2efcaa0c
icelessusr <iceless.usr@gmail.com>
2025-08-26 21:09:30 +0800

    Update for Json Project Definition

---
+++ Data/Scripts/projects.js
@@ -0,0 +1,46 @@
+async function ProjectHTML(name, description, title, subtitle, action, url, iconPath, bannerPath, color) {    
+    return `
+        <div class="project" style="background-color: ${color}">
+            <div class="project-filter" style="background-image: linear-gradient(45deg, ${color}, rgba(0,0,0,0) 70%);"></div>
+            <div class="project-title-area">
+                <h5 class="project-action">${action}</h5>
+                <h1 class="project-title">${title}</h1>
+                <h5 class="project-subtitle">${subtitle}</h5>
+            </div>
+            <div class="project-banner" style="background-image: url('${bannerPath}')"></div>
+            <div class="project-bottom">
+                <div class="project-icon" style="background-image: url('${iconPath}');"></div>
+                <h4 class="project-name">${name}</h4>
+                <p class="project-description">${description}</p>
+                <a class="project-view" href="${url}">VIEW</a>
+            </div>
+        </div>
+    `;
+}
+
+async function ProjectsHTML(projects) {
+    let html = '';
+    for (const id in projects) {
+        const project = projects[id];
+        html += await ProjectHTML(
+            project.name,
+            project.description,
+            project.title,
+            project.subtitle,
+            project.action,
+            project.url,
+            `Data/Icons/icon.${id}.svg`,
+            `Data/Images/project.${id}.banner.png`,
+            project.color
+        );
+    }
+    return html;
+}
+
+async function ProjectsHTMLFromURL(url) {
+    return await fetch(url)
+        .then(response => response.json())
+        .then(projects => ProjectsHTML(projects));
+}
+
+export { ProjectsHTMLFromURL };
