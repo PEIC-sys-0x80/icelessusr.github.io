bbe4828959d4c74f3b2d0a543ac04d8dec346ad5
icelessusr <iceless.usr@gmail.com>
2025-10-11 19:13:14 +0800

    Fix: project.html Add: eatkano.html

--- Data/Scripts/projects.js
+++ Data/Scripts/projects.js
@@ -2,17 +2,17 @@ async function ProjectHTML(name, description, title, subtitle, action, infourl, 
     return `
         <div class="project" style="background-color: ${color}">
             <div class="project-filter" style="background-image: linear-gradient(45deg, ${color}, rgba(0,0,0,0) 70%);"></div>
-            <div class="project-title-area">
+            <a href="${infourl}" class="project-title-area">
                 <h5 class="project-action">${action}</h5>
                 <h1 class="project-title">${title}</h1>
                 <h5 class="project-subtitle">${subtitle}</h5>
-            </div>
+            </a>
             <div class="project-banner" style="background-image: url('${bannerPath}')"></div>
             <div class="project-bottom">
                 <div class="project-icon" style="background-image: url('${iconPath}');"></div>
                 <h4 class="project-name">${name}</h4>
                 <p class="project-description">${description}</p>
-                <a class="project-view" rel="nofollow noreferrer noopener" href="${url}">VIEW</a>
+                <a class="project-view" rel="nofollow noreferrer noopener" href="${url}">GET</a>
             </div>
         </div>
     `;
@@ -28,7 +28,7 @@ async function ProjectsHTML(projects, owner) {
             project.title,
             project.subtitle,
             project.action,
-            project.infourl || project.url,
+            project.infourl || `projects?focus=${owner}.${id}`,
             project.url,
             project.icon || `/Data/Icons/icon.${id}.svg`,
             project.banner || `/Data/Images/project.${id}.banner.png`,
